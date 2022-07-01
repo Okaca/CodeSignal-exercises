83834bbfcedf2409d2597ab8173858230ed910d6
Okaca <79399112+Okaca@users.noreply.github.com>
2022-07-01 21:16:54 +0300

    Create MakeArrayConsecutive2.py

---
+++ MakeArrayConsecutive2.py
@@ -0,0 +1,40 @@
+"""
+Ratiorg got statues of different sizes as a present from CodeMaster for his birthday, each statue having an non-negative integer size. Since he likes to make things perfect, he wants to arrange them from smallest to largest so that each statue will be bigger than the previous one exactly by 1. He may need some additional statues to be able to accomplish that. Help him figure out the minimum number of additional statues needed.
+
+Example
+
+For statues = [6, 2, 3, 8], the output should be
+solution(statues) = 3.
+
+Ratiorg needs statues of sizes 4, 5 and 7.
+
+Input/Output
+
+[execution time limit] 4 seconds (py3)
+
+[input] array.integer statues
+
+An array of distinct non-negative integers.
+
+Guaranteed constraints:
+1 ≤ statues.length ≤ 10,
+0 ≤ statues[i] ≤ 20.
+
+[output] integer
+
+The minimal number of statues that need to be added to existing statues such that it contains every integer size from an interval [L, R] (for some L, R) and no other sizes.
+"""
+
+def solution(statues):
+    sorted_list = sorted(statues)
+    
+    num = 0
+    
+    for i in range(sorted_list[-1] - sorted_list[0]):
+        if sorted_list[i+1] != sorted_list[i] + 1:
+            num += 1
+            sorted_list.insert(i+1, sorted_list[i] + 1)
+    
+    print(sorted_list)
+    
+    return num
