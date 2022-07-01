21e3eb5a184f0ecbe055e40b6a891cb4850f75dc
Okaca <79399112+Okaca@users.noreply.github.com>
2022-07-01 21:13:16 +0300

    Create adjacentElementsProduct.py

---
+++ adjacentElementsProduct.py
@@ -0,0 +1,35 @@
+"""
+Given an array of integers, find the pair of adjacent elements that has the largest product and return that product.
+
+Example
+
+For inputArray = [3, 6, -2, -5, 7, 3], the output should be
+solution(inputArray) = 21.
+
+7 and 3 produce the largest product.
+
+Input/Output
+
+[execution time limit] 4 seconds (py3)
+
+[input] array.integer inputArray
+
+An array of integers containing at least two elements.
+
+Guaranteed constraints:
+2 ≤ inputArray.length ≤ 10,
+-1000 ≤ inputArray[i] ≤ 1000.
+
+[output] integer
+
+The largest product of adjacent elements.
+"""
+
+def solution(inputArray):
+    import math
+    maxval = -math.inf
+    for i in range(len(inputArray)-1):
+        a = inputArray[i] * inputArray[i+1]
+        if a > maxval:
+            maxval = a
+    return maxval
